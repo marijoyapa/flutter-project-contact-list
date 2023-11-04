65f8942c642907e192011e992ce63adf33772cfb
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-11-04 23:24:10 +0800

    added darktheme

--- contact_list/lib/main.dart
+++ contact_list/lib/main.dart
@@ -10,6 +10,15 @@ final colorScheme = ColorScheme.fromSeed(
   primaryContainer: const Color.fromARGB(255, 40, 40, 40),
 );
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 final theme = ThemeData().copyWith(
   useMaterial3: true,
   scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0),
@@ -24,6 +33,20 @@ final theme = ThemeData().copyWith(
     titleLarge: GoogleFonts.robotoFlex(
       fontWeight: FontWeight.bold,
     ),
+  ),
+);
+
+final darkTheme = ThemeData.dark().copyWith(
+  useMaterial3: true,
+  colorScheme: kDarkColorScheme,
+  cardTheme: const CardTheme().copyWith(
+    color: kDarkColorScheme.secondaryContainer,
+    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+        backgroundColor: kDarkColorScheme.primaryContainer,
+        foregroundColor: kDarkColorScheme.onPrimaryContainer),
   ),
 );
 
