17a7b57d208b9034f27f4e359f973f2d61047b0f
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-11-10 10:13:39 +0800

    call number selection for multiple numbers

--- contact_list/lib/widgets/contact_details/action_icon.dart
+++ contact_list/lib/widgets/contact_details/action_icon.dart
@@ -23,27 +23,71 @@ class ActionIcons extends StatelessWidget {
         decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.primaryContainer,
             borderRadius: BorderRadius.circular(10)),
-        child: 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                color: state ? Colors.blue : Theme.of(context).iconTheme.color!.withOpacity(0.3),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: state
+                  ? Colors.blue
+                  : Theme.of(context).iconTheme.color!.withOpacity(0.3),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              label,
+              style: TextStyle(
+                color: state
+                    ? Colors.blue
+                    : Theme.of(context).iconTheme.color!.withOpacity(0.3),
               ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                label,
-                style: TextStyle(
-                  color: state ? Colors.blue : Theme.of(context).iconTheme.color!.withOpacity(0.3),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void onCall(String number) async {
+    await FlutterPhoneDirectCaller.callNumber(number);
+  }
+
+  void onTapCall(BuildContext context) {
+    if (numberList.length == 1) {
+      String num = numberList[0].digit;
+      onCall(num);
+    } else if (numberList.length > 1) {
+      showDialog(
+        context: context,
+        builder: (context) => Dialog(
+          insetPadding: const EdgeInsets.symmetric(horizontal: 5),
+          backgroundColor: Theme.of(context).primaryColor,
+          child: SizedBox(
+            width: 300,
+            height: numberList.length * 60,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: numberList
+                  .map(
+                    (number) => RadioListTile(
+                      dense: true,
+                      value: number,
+                      groupValue: null,
+                      onChanged: (val) {
+                        NumberList newNum = val!;
+                        Navigator.of(context).pop();
+                        onCall(newNum.digit);
+                      },
+                      title: Text("${number.digit} (${number.typeName.name})"),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -60,8 +104,8 @@ class ActionIcons extends StatelessWidget {
         iconContainer(
             icon: Icons.call_sharp,
             label: 'Call',
-            onCall: () async {
-              await FlutterPhoneDirectCaller.callNumber(numberList[0].digit);
+            onCall: () {
+              onTapCall(context);
             },
             state: true,
             context: context),
